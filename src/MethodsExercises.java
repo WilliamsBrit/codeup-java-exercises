15ad374f82f560606de239d21e6112cdd4a4e430
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-12-13 16:34:01 -0600

    started working on Methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,30 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+    }
+
+
+public static int addition(int sum1, int sum2){
+    return sum1 + sum2;
+        }
+
+    public static int subtraction(int sub1, int sub2){
+        return sub1 - sub2;
+    }
+    public static int multiplication(int mul1, int mul2){
+        return mul1 * mul2;
+    }
+
+    public static int division(int div1, int div2){
+        return div1 % div2;
+    }
+
+
+    public static int multiply(int multi1, int multi2){
+
+        if() {
+
+        }
+
+    }
+}
