632a19cad7233a2489f54ea6980e03174c166f0a
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-12-14 14:35:05 -0600

    bob and String exercising upload

---
+++ src/Bob.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter input.");
+//        String userInput = sc.next();
+//
+//        if (userInput.trim().endsWith("?")) {
+//            System.out.println("Sure.");
+//        } else if (userInput.trim().endsWith("!")) {
+//            System.out.println("Whoa,chill out!");
+//        } else if (userInput.isEmpty()) {
+//            System.out.println("Fine. Be that way!");
+//        } else {
+//            System.out.println("Whatever.");
+//        }
+//sc.close();
+
+//        boolean confirm;
+//        do {
+//            System.out.println("What would you like to ask Bob?");
+//            String userInput = sc.nextLine();
+//
+//            if (userInput.trim().endsWith("?")) {
+//                System.out.println("Sure.");
+//            } else if (userInput.trim().endsWith("!")) {
+//                System.out.println("Whoa,chill out!");
+//            } else if (userInput.isEmpty()) {
+//                System.out.println("Fine. Be that way!");
+//            } else {
+//                System.out.println("Whatever.");
+//            }
+//
+//            System.out.println("Continue [y/n");
+//            confirm = sc.nextLine().equalsIgnoreCase("y");
+//
+//        } while (confirm);
+    }
+
+
+
+}
+
---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main (String[] args){
+//        String output = "We don't need no education\nWe don't need no thought control";
+//        System.out.println(output);
+
+//        String output = "\"Check \"this\" out!, \"s inside of \"s!\"";
+//        System.out.println(output);
+
+//        String output = "In windows, the main drive is usually C:\\";
+//        System.out.println(output);
+
+//        String output = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash\\\\\\!";
+//        System.out.println(output);
+
+    }
+}
